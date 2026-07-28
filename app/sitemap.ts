a61d20ae679941b858151c0ceae4831e0109063e
seasidemedialv <info@seasidemedia.lv>
2026-07-28 17:04:13 +0300

    Make sitemap route compatible with static export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { seoPages } from "@/lib/site-data";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = "https://virtuvespecpasutijuma.lv";
   const staticRoutes = [
